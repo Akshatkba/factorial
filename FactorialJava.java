a6dd8fcb80cd41d8ffd3bcf30953d0ba94cb256b
Akshat Bansal <95624168+Akshatkba@users.noreply.github.com>
2022-10-31 21:50:40 +0530

    Update FactorialJava.java
    factorial solution using recursion way

--- FactorialJava.java
+++ FactorialJava.java
@@ -1,13 +1,21 @@
-import java.io.*;
-
-class FactorialJava {
-	public static void main (String[] args) {
-		
-		int ans=1, n=6;
-		for (int i=1; i<=n; i++) {
-		    ans=ans*i;
-		}
-		System.out.println(ans);
-		
-	}
+import java.util.Scanner;
+public class factorial {
+    // factorial method using recursion
+        static int factorials(int x){
+            int fact = 1;
+            if(x==0||x==1){
+                return 1;
+            }
+            else{
+                fact = x * factorials(x-1);
+                return fact;
+            }
+        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int a = sc.nextInt();
+        int ans = factorials(a);
+        System.out.println(ans);
+    }
 }
